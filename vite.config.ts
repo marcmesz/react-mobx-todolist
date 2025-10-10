9d94981768038d23479cf319ff67948953323a7e
Mészöly Márton <meszoly.marton@gmail.com>
2025-10-10 18:02:45 +0200

    update deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,29 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
-  base: "/react-mobx-todo-list/"
+  base: "/react-mobx-todolist/",
+  build: {
+    outDir: "build",
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: "static/assets/[name].[hash].js",
+        chunkFileNames: "static/assets/[name].[hash].js",
+        assetFileNames: (assetInfo) => {
+          const ext = assetInfo.names?.[0]
+          if (
+            ["png", "jpg", "jpeg", "gif", "svg", "ico", "webp"].includes(
+              ext ?? ""
+            )
+          ) {
+            return "static/images/[name].[hash][extname]"
+          }
+          if (["css"].includes(ext ?? "")) {
+            return "static/assets/[name].[hash][extname]"
+          }
+          return "static/assets/[name].[hash][extname]"
+        }
+      }
+    }
+  }
 })
